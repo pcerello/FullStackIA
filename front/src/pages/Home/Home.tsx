e69641858070f5f244c2c2ee12b9102b871412e8
Ikari-421 <maximilien.auclair@gmail.com>
2025-01-24 16:20:29 +0100

    Final de l'historique sur la page home

--- front/src/pages/Home/Home.tsx
+++ front/src/pages/Home/Home.tsx
@@ -4,89 +4,90 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import ApiService from "../../services/ApiService";
 import { useNavigate } from "react-router-dom";
-import enqueteImage from "../../assets/enquete.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClockRotateLeft } from "@fortawesome/free-solid-svg-icons";
+import ModalHome from "../../components/ModalHome/ModalHome";
 
-function Home() {
+const Home: React.FC = () => {
   const navigate = useNavigate();
-  const [theme, setTheme] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [historiques, setHistoriques] = useState<{ id: 0, description: "" }[]>([]);
-  const [isModalHistorique, setIsModalHistorique] = useState(false);
+  const [theme, setTheme] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [historiques, setHistoriques] = useState<{ id: number; description: string }[]>([]);
+  const [isModalHistorique, setIsModalHistorique] = useState<boolean>(false);
 
   const handleThemeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTheme(event.target.value);
   };
 
-  async function fetchData(question: String) {
+  const fetchData = async (question: string) => {
     try {
-        setIsLoading(true);
-        const data = await ApiService.post("genererScenario", question);
-        console.log(data);
-        navigate("/game/" + data.id );
+      setIsLoading(true);
+      const data = await ApiService.post("genererScenario", question);
+      console.log(data);
+      navigate("/game/" + data.id);
     } catch (error) {
-        console.error("Erreur lors de l'appel API :", error);
+      console.error("Erreur lors de l'appel API :", error);
     } finally {
       setIsLoading(false);
     }
-  }
+  };
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
     fetchData(theme);
   };
 
-  async function fetchHistorique() {
+  const fetchHistorique = async () => {
     try {
-      console.log("Fetching Temoignages...");
-      const temoignages = await ApiService.get(`historiqueScenarios`);
+      console.log("Fetching Historique...");
+      const temoignages = await ApiService.get("historiqueScenarios");
       setHistoriques(temoignages);
-      console.log(temoignages);
-
-
     } catch (error) {
       console.error("Erreur lors de l'appel API :", error);
     }
-  }
+  };
 
   const handleListHistoriques = () => {
-    console.log("Liste des 5 dernière histoire");
     setIsModalHistorique(true);
     fetchHistorique();
   };
 
+  const closeModal = () => {
+    setIsModalHistorique(false);
+  };
+
   return (
-      <div className="Home">
-        <Header />
-        <div className="Body-home">
-          <div className="form-overlay">
-            <h2>Saisissez le thème de votre enquête</h2>
-            <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                value={theme}
-                onChange={handleThemeChange}
-                placeholder="Ex: Meurtre mystérieux"
-                disabled={isLoading} // Désactiver l'input pendant le chargement
-                />
-                <button type="submit" disabled={isLoading}>
-                  {isLoading ? (
-                    <span className="loader">Attendez... longtemps... très longtemps...</span>
-                  ) : (
-                    "Lancer la partie"
-                  )}
-                </button>
-                <button onClick={() => handleListHistoriques()}>
-                    <FontAwesomeIcon icon={faClockRotateLeft} />
-                    Historique
-                </button> 
-            </form>
-          </div>
+    <div className="Home">
+      <Header />
+      <div className="Body-home">
+        <div className="form-overlay">
+          <h2>Saisissez le thème de votre enquête</h2>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={theme}
+              onChange={handleThemeChange}
+              placeholder="Ex: Meurtre mystérieux"
+              disabled={isLoading}
+            />
+            <button type="submit" disabled={isLoading}>
+              {isLoading ? (
+                <span className="loader">Attendez... longtemps... très longtemps...</span>
+              ) : (
+                "Lancer la partie"
+              )}
+            </button>
+          </form>
+          <button onClick={handleListHistoriques}>
+            <FontAwesomeIcon icon={faClockRotateLeft} />
+            Historique
+          </button>
         </div>
-        <Footer />
       </div>
+      <Footer />
+      <ModalHome isOpen={isModalHistorique} onClose={closeModal} data={historiques} />
+    </div>
   );
-}
+};
 
 export default Home;
